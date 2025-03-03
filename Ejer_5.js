ddf6b33eff75667ce098417b5022059941f09f81
BE <KAG96@outlook.es>
2025-03-03 11:53:20 +0000

    último

--- Ejer_5.js
+++ Ejer_5.js
@@ -1,7 +1,7 @@
 //Solicita al usuario una letra, si inserta una a muestra el número 7, si es una b, el 9, si es una c el 101 y si no es ninguno de los tres, indícale que se ha equivocado de letra
 let letra = prompt('Inserte una letra');
 
-if (letra === 'a') 
+(letra === 'a') 
     {alert("7");}
     else if (letra === 'b') 
     {alert("9");}
@@ -9,3 +9,25 @@ if (letra === 'a')
     {alert("101");}
     else 
     {alert("Se ha equivocado de letra")}
+
+   /* let letra;
+do{
+  letra = prompt('Inserta una letra');
+}while((letra < 'A' || letra > 'Z') 
+  || (letra < 'a' || letra > 'z'));
+
+
+switch (letra) {
+  case 'a':
+    alert('7');
+    break;
+  case 'b':
+    alert('9');
+    break;
+  case 'c':
+    alert('9');
+    break;
+  default:
+    alert('Se ha equivocado de letra');
+}
+
--- Ejer_6.js
+++ Ejer_6.js
@@ -1,1 +1,12 @@
 //Ordena alfabéticamente un array con 7 palabras puedes usar el algoritmo de la burbuja
+let palabras = ["manzana", "perro", "gato", "piña", "piedra", "gato", "casa", "animal"];
+
+for (let i = 0; i < palabras.length - 1; i++) {
+    for (let j = 0; j < palabras.length - 1 - i; j++) {
+        if (palabras[j] > palabras[j + 1]) {
+            let temp = palabras[j];
+            palabras[j] = palabras[j + 1];
+            palabras[j + 1] = temp;
+        }
+    }
+}
