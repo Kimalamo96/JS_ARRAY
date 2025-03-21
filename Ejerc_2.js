906bb1700e242fd8924992af1aa4bea0890e8e0c
Kimalamo96 <KIMALAMO.GR@GMAIL.COM>
2025-03-21 12:27:40 +0000

    Update Ejerc_2.js

--- Ejerc_2.js
+++ Ejerc_2.js
@@ -1,6 +1,6 @@
 //Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.
-let frase = prompt("Ingresa una frase:");
-let letra = prompt("Ingresa una letra:");
+let frase = prompt("Ingresa una frase: ");
+let letra = prompt("Ingresa una letra: ");
 let contador = 0;
 
 for (let i = 0; i < frase.length; i++) {
