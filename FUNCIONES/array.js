6a301e89229e861c7c881bcb38ce02633ad1b28a
BE <KAG96@outlook.es>
2025-04-17 22:50:26 +0100

    ORGANIZACI´ÓN DE CARPETAS

--- FUNCIONES/array.js
+++
@@ -1,4 +0,0 @@
-<input type="button" value="AÃ±adir" 
-onclick="let frutas = ['Manzana', 'Banana']; frutas.push('Fresa'); alert(frutas);">
-<input type="button" value="Borrar" 
-onclick="let frutas = ['Manzana', 'Banana']; frutas.pop(); alert(frutas);">
--- FUNCIONES/saludos.js
+++
@@ -1,2 +0,0 @@
-<input type="button" value="Saludar" onclick="alert('Hola!!')">
-    <input type="button" value="Despedir" onclick="alert('Adiós!!')">
